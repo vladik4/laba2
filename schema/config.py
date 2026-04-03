478d7d6a1f730e2d3709663ed139190cae6f9411
vladik4 <138467699+vladik4@users.noreply.github.com>
2026-04-03 16:37:33 +0300

    Create config.py

---
+++ schema/config.py
@@ -0,0 +1,15 @@
+import os
+
+def try_parse(type, value: str):
+    try:
+        return type(value)
+    except Exception:
+        return None
+
+# MQTT config
+MQTT_BROKER_HOST = os.environ.get('MQTT_BROKER_HOST') or 'test.mosquitto.org'
+MQTT_BROKER_PORT = try_parse(int, os.environ.get('MQTT_BROKER_PORT')) or 1883
+MQTT_TOPIC = os.environ.get('MQTT_TOPIC') or 'nubip/road/agent_data'
+
+# Delay for sending data to mqtt in seconds
+DELAY = try_parse(float, os.environ.get('DELAY')) or 1
