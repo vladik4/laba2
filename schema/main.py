faf0446a350af35bb93046f17a41d962ba8017d5
vladik4 <138467699+vladik4@users.noreply.github.com>
2026-04-03 16:38:15 +0300

    Implement MQTT client and data publishing

---
+++ schema/main.py
@@ -0,0 +1,46 @@
+from paho.mqtt import client as mqtt_client
+import time
+from schema.aggregated_data_schema import AggregatedDataSchema
+from file_datasource import FileDatasource
+import config
+
+def connect_mqtt(broker, port):
+    """Create MQTT client"""
+    print(f"CONNECT TO {broker}:{port}")
+    
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print(f"Connected to MQTT Broker ({broker}:{port})!")
+        else:
+            print(f"Failed to connect {broker}:{port}, return code {rc}")
+            exit(rc)
+    
+    client = mqtt_client.Client()
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    client.loop_start()
+    return client
+
+def publish(client, topic, datasource, delay):
+    datasource.startReading()
+    while True:
+        time.sleep(delay)
+        data = datasource.read()
+        msg = AggregatedDataSchema().dumps(data)
+        result = client.publish(topic, msg)
+        status = result[0]
+        if status == 0:
+            print(f"Send data to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+
+def run():
+    # Prepare mqtt client
+    client = connect_mqtt(config.MQTT_BROKER_HOST, config.MQTT_BROKER_PORT)
+    # Prepare datasource
+    datasource = FileDatasource("data/accelerometer.csv", "data/gps.csv")
+    # Infinity publish data
+    publish(client, config.MQTT_TOPIC, datasource, config.DELAY)
+
+if __name__ == '__main__':
+    run()
